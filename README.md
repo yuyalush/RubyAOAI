6ffb1b94a10d91597a3e71f9d6aaaa7f25be3f32
Yuya Yoshida <728718+yuyalush@users.noreply.github.com>
2024-10-25 06:47:59 +0000

    Create Readme, Fix main script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RubyAOAI
+
+RubyからAzure OpenAI Serviceを呼ぶ一番シンプルなサンプルコードです。
+
+alexrudall/ruby-openai を使うと超簡単
+
+## 手順
+
+- Azure OpenAI Serviceをデプロイし、ポータルからエンドポイントとKEYを取得する
+- dotenvを .env にリネームし、取得した値を書き込む(リポジトリに保存しないように注意)
+- bundle installでGemをセットアップ
+- 実行する
--- rubyaoai.rb
+++ rubyaoai.rb
@@ -4,7 +4,7 @@ require 'openai'
 # Azure OpenAI Serviceの設定を環境変数から読み込む
 OpenAI.configure do |config|
     config.access_token = ENV.fetch("AZURE_OPENAI_API_KEY")  # APIキーを設定
-    config.uri_base = ENV.fetch("AZURE_OPENAI_URI")  # エンドポイントURIを設定
+    config.uri_base = ENV.fetch("AZURE_OPENAI_URI")  # エンドポイントURIとモデルを設定
     config.api_type = :azure  # APIタイプをAzureに設定
     config.api_version = "2023-03-15-preview"  # APIバージョンを設定
     config.log_errors = true
@@ -13,8 +13,7 @@ end
 # チャットリクエストを送信
 response = OpenAI::Client.new().chat(
   parameters: {
-    model: "gpt-4o", # モデルを指定
-    messages: [{ role: "user", content: "こんにちは！ごきげんはいかが？"}], # メッセージを設定
+    messages: [{ role: "user", content: "Azure OpenAI Service の概要を説明して"}], # メッセージを設定
     temperature: 0.7, # 応答の多様性を設定
   }
 )
